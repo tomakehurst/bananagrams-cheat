3c5318eb88bba53865596f62a075fea00f400025
Tom Akehurst <t.m.akehurst@googlemail.com>
2013-02-10 17:47:27 +0000

    Removed unused functions

--- test/bananagrams_cheat/words_test.clj
+++ test/bananagrams_cheat/words_test.clj
@@ -20,15 +20,5 @@
 (fact "First word of specific length makeable from letters"
   (first-word-makeable-with-letters [\t \b \i \e \f \f \c \y \u \e] 6) => "buffet")
 
-(fact "Deduct from letter counts"
-  (deduct-from-letter-counts { \s 3
-                               \u 1
-                               \m 2
-                               \e 3
-                               \r 6
-                               \d 2
-                               \f 1}
-                               "summer") => {\s 2 \e 2 \r 5 \d 2 \f 1})
-
 (fact "Remove letters in word"
   (remove-letters-in-word "thing" [\g \g \i \n \t \d \r \h \b]) => [\g \d \r \b])
